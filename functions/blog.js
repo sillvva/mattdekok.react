c679b2e65da7dac12a3eba90240bf49f0a241934
Matt Dekok <matt.dekok@gmail.com>
2022-05-18 11:50:54 -0500

    Improve fetchPosts function to only update changed files

--- functions/blog.js
+++ functions/blog.js
@@ -6,38 +6,55 @@ const firebase = require('./firebase.js');
 const storageContent = "blog/articles";
 
 async function fetchPosts() {
-  const { storage, ref, getDownloadURL, getMetadata, list } = firebase;
-  const { firestore, doc, setDoc } = firebase;
+  const { storage, ref, getDownloadURL, getMetadata, list, statSync } = firebase;
+  const { firestore, doc, setDoc, getDoc } = firebase;
 
   const storageRef = ref(storage, storageContent);
   const contentList = await list(storageRef);
 
-  console.log(`Found ${contentList.items.length} items`);
+  console.log(`Found ${contentList.items.length} files`);
 
-  const files = {};
+  const docRef = doc(firestore, `${storageContent}`);
+  const store = await getDoc(docRef);
+  const files = store.data() || {};
+
+  let changes = 0;
   for(const file of contentList.items) {
     const metadata = await getMetadata(file);
-    const url = await getDownloadURL(file);
+    const timeCreated = new Date(metadata.timeCreated).toISOString();
     const ext = path.extname(metadata.name);
     const slug = path.basename(metadata.name);
     if (ext != '.md') continue;
-
-    console.log(`Parsing: ${slug}`);
     
+    const fsItem = files[slug];
+    if (fsItem && (!fsItem.timeCreated || timeCreated > fsItem.timeCreated)) {
+      console.log(`Updating: ${slug}`);
+      changes++;
+    }
+    else if (!fsItem) {
+      console.log(`Adding: ${slug}`);
+      changes++;
+    }
+    else continue;
+    
+    const url = await getDownloadURL(file);
     const result = await axios.get(url);
     const parsedMD = matter(result.data);
     files[slug] = {
       name: metadata.name,
       path: metadata.fullPath,
       url: url,
+      timeCreated: timeCreated.toISOString(),
       data: parsedMD.data
     };
   }
   
-  console.log('Storing metadata to Firestore');
-  
-  const docRef = doc(firestore, `${storageContent}`);
-  await setDoc(docRef, files);
+  if (changes) {
+    console.log('Storing metadata to Firestore');
+    await setDoc(docRef, files);
+  }
+  else console.log('No changes found');
+  process.exit();
 }
 
 module.exports = {
