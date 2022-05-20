68de904ea88210b0c2b81c47bc334bf3f02f1df9
Matt Dekok <matt.dekok@gmail.com>
2022-05-20 08:40:38 -0500

    Use Firebase Firestore on Experience

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -1,112 +1,39 @@
 import type { NextPage } from 'next'
+import { PropsWithChildren } from 'react';
 import { menuItems } from '../store/main-layout.context';
 import PageHeader from '../components/page-header'
 import Page from '../components/page';
 import Layout from '../layouts/layout';
 import PageMeta from '../components/meta';
+import { doc, getDoc, setDoc } from '../functions/firebase'
 
-const Experience: NextPage = () => {
+const Experience: NextPage<ExperienceProps> = (props: PropsWithChildren<ExperienceProps>) => {
   return (
     <Layout>
       <PageMeta title="Experience" />
       <PageHeader title="Experience" items={menuItems} />
       <Page.Body>
         <Page.Article className="w-full md:w-9/12 lg:w-9/12 xl:w-8/12 2xl:w-7/12">
-          <Page.Section>
-            <h2 className="text-xl font-bold">Work Experience</h2>
-            <Page.SectionItems>
-              <Page.SectionItem image="/images/ddb.png">
-                <h3>Forum, Twitch, and Discord Moderator</h3>
-                <h4>
-                  <a href="https://www.dndbeyond.com/" target="_blank" rel="noreferrer noopener"> D&amp;D Beyond </a>
-                </h4>
-                <h5>2019 - Present</h5>
-              </Page.SectionItem>
-              <Page.SectionItem image="/images/valmont.jpg">
-                <h3>Front End Web Developer</h3>
-                <h4>
-                  <a href="http://www.valmont.com/" target="_blank" rel="noreferrer noopener"> Valmont Industries, Inc. </a>
-                </h4>
-                <h5>2019 - Present</h5>
-              </Page.SectionItem>
-              <Page.SectionItem image="/images/agsense.png">
-                <h3>Web Developer</h3>
-                <h4>
-                  <a href="http://agsense.com/" target="_blank" rel="noreferrer noopener"> AgSense, LLC. </a>
-                </h4>
-                <h5>2012 - Present</h5>
-              </Page.SectionItem>
-            </Page.SectionItems>
-          </Page.Section>
-          <Page.Section>
-            <h2 className="text-xl font-bold">Education</h2>
-            <Page.SectionItems>
-              <Page.SectionItem image="/images/dsu.png">
-                <h3>Computer and Information Systems Security/Information Assurance</h3>
-                <h4>
-                  <a href="https://dsu.edu/academics/degrees-and-programs/computer-information-systems-bs" target="_blank" rel="noreferrer noopener"> Dakota State University </a>
-                </h4>
-                <h5>Bachelor of Science Degree</h5>
-              </Page.SectionItem>
-              <Page.SectionItem image="/images/mti.png">
-                <h3>Computer Software and Media Applications</h3>
-                <h4>
-                  <a href="https://www.mitchelltech.edu/academics/programs-we-offer/online/administrative-office-specialist" target="_blank" rel="noreferrer noopener">
-                    Mitchell Technical Institute
-                  </a>
-                </h4>
-                <h5>Associate&apos;s Degree</h5>
-              </Page.SectionItem>
-            </Page.SectionItems>
-          </Page.Section>
-          <Page.Section>
-            <h2 className="text-xl font-bold">Licenses &amp; Certifications</h2>
-            <Page.SectionItems>
-              <Page.SectionItem image="/images/mos.png">
-                <h3>Microsoft Office Specialist</h3>
-                <h4>Microsoft Excel - Mitchell Tech</h4>
-                <h5>
-                  <a href="https://www.microsoft.com/en-us/learning/dashboard.aspx" target="_blank" rel="noreferrer noopener"> Credential ID 6154048 </a>
-                </h5>
-              </Page.SectionItem>
-              <Page.SectionItem image="/images/udemy.svg">
-                <h3>
-                  <a href="https://www.udemy.com/react-the-complete-guide-incl-redux/" target="_blank" rel="noreferrer noopener"> React - The Complete Guide </a>
-                </h3>
-                <h4>Udemy - Academind by Maximilian Schwarzmülle</h4>
-                <h5>
-                  <a target="_blank" rel="noreferrer noopener" href="http://ude.my/UC-WSZBKXEW"> Certificate </a>
-                </h5>
-              </Page.SectionItem>
-              <Page.SectionItem image="/images/udemy.svg">
-                <h3>
-                  <a href="https://www.udemy.com/course/nextjs-react-the-complete-guide/" target="_blank" rel="noreferrer noopener"> Next.js &amp; React - The Complete Guide</a>
-                </h3>
-                <h4>Udemy - Academind by Maximilian Schwarzmülle</h4>
-                <h5>
-                  <a target="_blank" rel="noreferrer noopener" href="http://ude.my/UC-9057120f-61f0-480b-aaf0-8b3e77b095bb"> Certificate </a>
-                </h5>
-              </Page.SectionItem>
-              <Page.SectionItem image="/images/udemy.svg">
-                <h3>
-                  <a href="https://www.udemy.com/course/understanding-typescript/" target="_blank" rel="noreferrer noopener"> Understanding TypeScript </a>
-                </h3>
-                <h4>Udemy - Academind by Maximilian Schwarzmülle</h4>
-                <h5>
-                  <a target="_blank" rel="noreferrer noopener" href="http://ude.my/UC-DIDRRWV8"> Certificate </a>
-                </h5>
-              </Page.SectionItem>
-              <Page.SectionItem image="/images/udemy.svg">
-                <h3>
-                  <a href="https://www.udemy.com/nuxtjs-vuejs-on-steroids/" target="_blank" rel="noreferrer noopener"> Nuxt.js - Vue.js on Steroids </a>
-                </h3>
-                <h4>Udemy - Academind by Maximilian Schwarzmülle</h4>
-                <h5>
-                  <a target="_blank" rel="noreferrer noopener" href="http://ude.my/UC-T5TFG77X"> Certificate </a>
-                </h5>
-              </Page.SectionItem>
-            </Page.SectionItems>
-          </Page.Section>
+          {props.experience.map((section) => (
+            <Page.Section key={section.name}>
+              <h2 className="text-xl font-bold">{section.name}</h2>
+              <Page.SectionItems>
+                {section.experience.map((exp, i) => (
+                  <Page.SectionItem key={`${exp.name}${i}`} image={exp.image}>
+                    <h3>
+                      {!exp.nameLink ? exp.name : <a href={exp.nameLink} target="_blank" rel="noreferrer noopener">{exp.name}</a>}
+                    </h3>
+                    <h4>
+                      {!exp.h4Link ? exp.h4 : <a href={exp.h4Link} target="_blank" rel="noreferrer noopener">{exp.h4}</a>}
+                    </h4>
+                    <h5>
+                      {!exp.h5Link ? exp.h5 : <a href={exp.h5Link} target="_blank" rel="noreferrer noopener">{exp.h5}</a>}
+                    </h5>
+                  </Page.SectionItem>
+                ))}
+              </Page.SectionItems>
+            </Page.Section>
+          ))}
         </Page.Article>
       </Page.Body>
     </Layout>
@@ -114,3 +41,52 @@ const Experience: NextPage = () => {
 }
 
 export default Experience
+
+interface ExperienceItem {
+  name?: string;
+  nameLink?: string;
+  image: string;
+  h4: string;
+  h4Link?: string;
+  h5: string;
+  h5Link?: string;
+}
+
+interface ExperienceProps {
+  experience: ExperienceArrSection[]
+}
+
+interface ExperienceArrSection {
+  name: string;
+  experience: ExperienceItem[]
+}
+
+interface ExperienceSection {
+  [name: string]: ExperienceItem;
+}
+
+interface ExperienceSections {
+  [name: string]: ExperienceSection;
+}
+
+export async function getServerSideProps() {
+  const docRef = doc('website/experience');
+  const document = await getDoc(docRef);
+  const experience: ExperienceSections = document.data() || {};
+
+  return {
+    props: {
+      experience: Object.entries(experience)
+        .sort(([nameA], [nameB]) => nameA > nameB ? 1 : -1)
+        .map(([sectionName, section]) => ({
+          name: sectionName.replace(/^\d+\. /, ''),
+          experience: Object.entries(section)
+            .sort(([nameA], [nameB]) => nameA > nameB ? 1 : -1)
+            .map(([expName, exp]) => ({
+              name: expName.replace(/^\d+\. /, ''),
+              ...exp
+            }))
+        }))
+    }
+  }
+}
