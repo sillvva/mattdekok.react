6a2055188f098834898c696273452f9f3119f43e
Matt Dekok <matt.dekok@gmail.com>
2022-05-22 20:32:49 -0500

    Meta Fix

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -222,6 +222,11 @@ export async function getServerSideProps(context: any) {
   }
 
   const { content, data } = matter(result.data);
+  for (let key in data) {
+    if (data[key] instanceof Date) {
+      data[key] = data[key].toLocaleDateString('en-us', { weekday: "long", year: "numeric", month: "short", day: "numeric" });
+    }
+  }
   data.dateISO = new Date(data.date).toISOString();
   data.updatedISO = new Date(data.updated).toISOString();
 
