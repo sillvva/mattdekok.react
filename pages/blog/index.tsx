5dbc478f050a6c24443283739ca8d9ba4c508b89
Matt Dekok <matt.dekok@gmail.com>
2022-05-27 18:58:48 -0500

    useSWR for data fetching on page load

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,46 +1,33 @@
 import type { NextPage } from 'next'
-import { useEffect, useState } from 'react';
+import useSWR, { Fetcher } from 'swr';
 import Layout from '../../layouts/layout';
 import Page from '../../components/page';
 import BlogDirectory, { PostProps } from '../../components/blog';
 import PageMessage from '../../components/page-message';
-import { wait } from '../../functions/misc';
+
+const fetcher: Fetcher<{ posts: PostProps[] }> = (url: string) => fetch(url).then((res) => res.json());
 
 const Blog: NextPage = () => {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
+  const { data, error } = useSWR('/api/get-posts', fetcher, {
+    revalidateOnFocus: false,
+    revalidateIfStale: false
+  });
   const loaders: PostProps[] = Array(6).fill({
     title: "", date: "", image: "",
     description: "", slug: "", link: ""
   });
 
-  useEffect(() => {
-    wait(() => {
-      fetch('/api/get-posts')
-        .then(response => response.json())
-        .then(data => {
-          setPosts(data.posts || []);
-          setLoading(false);
-        })
-        .catch(error => {
-          setError(error);
-          setLoading(false);
-        });
-    }, 'Blog:get-posts', 50);
-  }, []);
-
   return (
     <Layout props={{ menu: true, meta: { title: "Blog" } }}>
       <Page.Body>
-        {loading ? (
-          <BlogDirectory posts={loaders} />
-        ) : error ? (
+        { error ? (
           <PageMessage>{error}</PageMessage>
-        ) : !posts.length ? (
+        ) : !data ? (
+          <BlogDirectory posts={loaders} />
+        ) : !(data.posts || []).length ? (
           <PageMessage>No posts found.</PageMessage>
         ) : (
-          <BlogDirectory posts={posts} />
+          <BlogDirectory posts={data.posts} />
         )}
       </Page.Body>
     </Layout>
