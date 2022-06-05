fd4b90addff5a5155c6f1745d04907a0596ea54c
Matt Dekok <matt.dekok@gmail.com>
2022-06-05 15:54:39 -0500

    Bug Fix

--- layouts/main/index.tsx
+++ layouts/main/index.tsx
@@ -63,7 +63,7 @@ export const mainMotion: { variants: Variants; transition: Transition } = {
   variants: {
     hidden: { opacity: 0 },
     enter: { opacity: 1 },
-    exit: { opacity: 0, transition: { duration: 0.1 } }
+    exit: { opacity: 0, transition: { duration: 0 } }
   },
   transition: {
     duration: 0.5
