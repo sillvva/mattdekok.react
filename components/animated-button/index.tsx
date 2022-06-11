22a02e7a924a9afd19dd17191284b3ca12f1dbce
Matt Dekok <matt.dekok@gmail.com>
2022-06-10 23:30:36 -0500

    useRipple

--- components/animated-button/index.tsx
+++ components/animated-button/index.tsx
@@ -1,10 +1,8 @@
-import dynamic from "next/dynamic";
 import Link from "next/link";
-import { Key, MouseEventHandler, useCallback, useRef, useState } from "react";
-import { wait } from "../../functions/misc";
+import { useRef, useState } from "react";
+import { useRipple } from "../ripple";
 import buttons from "./Buttons.module.scss";
-
-const Ripple = dynamic(() => import("./ripple"));
+import ripple from "../ripple/Ripple.module.scss";
 
 type AnimatedButtonProps = {
   link?: string;
@@ -31,10 +29,10 @@ export default function AnimatedButton(props: AnimatedButtonProps) {
     clickRipple
   } = props;
   const btnRef = useRef<HTMLAnchorElement>(null);
-  const [ripples, setRipples] = useState<Map<Key, JSX.Element>>(new Map());
+  const { ripples, mouseEvents, rippleClass } = useRipple({ enabled: clickRipple, active });
 
   const propClasses = itemClasses.map(c => buttons[c] ?? c);
-  const [classes, setClasses] = useState([buttons.Button, active ? buttons.Active : "", clickRipple ? buttons.Ripple : "", ...propClasses]);
+  const [classes, setClasses] = useState([buttons.Button, active ? buttons.Active : "", rippleClass, ...propClasses]);
   if (!classes.find(c => /Button\d+/.test(c))) {
     setClasses([...classes, buttons.Button5]);
   }
@@ -47,26 +45,9 @@ export default function AnimatedButton(props: AnimatedButtonProps) {
     ...(active && { cursor: "default" })
   } as React.CSSProperties;
 
-  const rippleUnload = () => {
-    wait(
-      () => {
-        setRipples(new Map());
-      },
-      "ripples",
-      600
-    );
-  };
-
-  const mouseHandler: MouseEventHandler<HTMLAnchorElement> = e => {
-    if (!clickRipple) return;
-    const key = Math.max(-1, ...[...ripples.keys()].map(r => parseInt(r.toString() || ""))) + 1;
-    const ripple = <Ripple key={key} index={key} onUnload={rippleUnload} x={e.nativeEvent.offsetX} y={e.nativeEvent.offsetY}></Ripple>;
-    setRipples(new Map(ripples).set(key, ripple));
-  };
-
   const btn = (
-    <a className={classes.join(" ")} style={style} ref={btnRef} onMouseDown={mouseHandler} onClick={e => active && e.preventDefault()}>
-      {[...ripples.values()]}
+    <a className={classes.join(" ")} style={style} ref={btnRef} {...mouseEvents}>
+      {ripples}
       {label}
     </a>
   );
